65a90277c895aa706beebdd8a084aa0381531e80
juseung-shin <jsshin6246@naver.com>
2024-09-23 17:08:22 +0900

    commit

---
+++ mypage/src/main/java/booklend/domain/BookCancled.java
@@ -0,0 +1,15 @@
+package booklend.domain;
+
+import booklend.infra.AbstractEvent;
+import java.time.LocalDate;
+import java.util.*;
+import lombok.Data;
+
+@Data
+public class BookCancled extends AbstractEvent {
+
+    private Long id;
+    private Date updateDt;
+    private Long borrowid;
+    private String status;
+}
