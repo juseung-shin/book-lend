f9348c93d28dfd78553087fb583327fd5d471ea6
juseung-shin <jsshin6246@naver.com>
2024-09-24 10:01:24 +0900

    commit

--- mypage/src/main/java/booklend/domain/BookApproved.java
+++ mypage/src/main/java/booklend/domain/BookApproved.java
@@ -9,7 +9,6 @@ import lombok.Data;
 public class BookApproved extends AbstractEvent {
 
     private Long id;
-    private String status;
     private Date updateDt;
     private Long borrowId;
     private String borrowStatus;
